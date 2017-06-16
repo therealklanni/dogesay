c1ac147214117f18850ed94fb973fdcdc874abc2
Kevin Lanni <therealklanni@gmail.com>
2017-06-15 22:23:57 -0700

    Support argument input

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-'use strict'
+const utf8 = require(`utf8-stream`)
+const map = require(`map-stream`)
+const { white, yellow, magenta, blue, cyan, green, red } = require(`chalk`)
 
-import utf8 from 'utf8-stream'
-import map from 'map-stream'
-import { white, yellow, magenta, blue, cyan, green, red } from 'chalk'
+const [y, w, Y, W, B, M, R, C, G] = [
+  yellow,
+  white,
+  yellow.bold,
+  white.bold,
+  blue.bold,
+  magenta.bold,
+  red.bold,
+  cyan.bold,
+  green.bold
+]
 
-let y = yellow
-let w = white
-let Y = yellow.bold
-let W = white.bold
-let B = blue.bold
-let M = magenta.bold
-let C = cyan.bold
-let G = green.bold
-let R = red.bold
-
-process.stdin
-  .pipe(utf8())
-  .pipe(map((buf, cb) => cb(null, buf.toString())))
-  .pipe(map((buf, cb) => {
-    let ln = (i) => buf.split('\n')[i] || ''
-    let doge = y(`
+const dogesay = (buf, cb) => {
+  const ln = i => buf.split(`\n`)[i] || ``
+  const doge = y(
+    `
                 Y.                      _
                 YiL                   .\`\`\`.
                 Yii;                .; .;;\`.
                 YY;ii._           .;\`.;;;; :
                 iiYYYYYYiiiii;;;;i\` ;;::;;;;               ${M(ln(0))}
             _.;YYYYYYiiiiiiYYYii  .;;.   ;;;                                        ${B(ln(1))}
-         .YY${Y('YYY')}YYYYYiiYYYYYYYYYYYYii;\`  ;;;;
-       .YYY${Y('YYYY$$')}YYiiYY$$${Y('$$iiiY')}YYYYY;.ii;\`..     ${C(ln(2))}
-      :YYY$?  \`TYiiYY${Y('$$$$$YYYYYYY')}iiYYYYiYYii.                        ${R(ln(3))}
-      Y$MM$:   :YYYYYY$!"\`\`"4${Y('YYYYY')}iiiYYYYiiYY.                                   ${W(ln(4))}
-   \`.Y:MM$$b.,dYY$$Yii" :'   :${Y('YYYY')+'llYiiYYYi'.replace(/Y+/g, (p) => w(p))}YYY   ${G(ln(5))}
-_.._Y:\`4MM$!YYYYYYYYYii,.__.diii$$${w('YYYYYYYYYYYY')};`+`                                ${B(ln(6))}
+         .YY${Y(`YYY`)}YYYYYiiYYYYYYYYYYYYii;\`  ;;;;
+       .YYY${Y(`YYYY$$`)}YYiiYY$$${Y(`$$iiiY`)}YYYYY;.ii;\`..     ${C(ln(2))}
+      :YYY$?  \`TYiiYY${Y(`$$$$$YYYYYYY`)}iiYYYYiYYii.                        ${R(ln(3))}
+      Y$MM$:   :YYYYYY$!"\`\`"4${Y(`YYYYY`)}iiiYYYYiiYY.                                   ${W(
+      ln(4)
+    )}
+   \`.Y:MM$$b.,dYY$$Yii" :'   :${Y(`YYYY`) + `llYiiYYYi`.replace(/Y+/g, p => w(p))}YYY   ${G(ln(5))}
+_.._Y:\`4MM$!YYYYYYYYYii,.__.diii$$${w(`YYYYYYYYYYYY`)};${`                                ${B(
+      ln(6)
+    )}
 .,._Y$b\`P\`     "4$$$$$iiiiiiii$$$$YY$$$$$$$YiY;              ${W(ln(7))}
    \`,Y\`$:       :$$$$$$$$$YYYYY$$$$$$$$$YYiiYYL                          ${Y(ln(8))}
     "Y;$$.    .;PPb$\`.,.\`\`T$$YY$$$$YYYYYYiiiYYU:      ${G(ln(9))}
@@ -42,8 +44,21 @@ _.._Y:\`4MM$!YYYYYYYYYii,.__.diii$$${w('YYYYYYYYYYYY')};`+`                     
       \`$$$$$$$YYYYYYYYYYYYY$$$$$$YYYYYYYYiiiYYYYYY               ${Y(ln(14))}
       .i!$$$$$$YYYYYYYYY$$$$$$YYY$$YYiiiiiiYYYYYYY                              ${W(ln(15))}
      :YYiii$$$$$$$YYYYYYY$$$$YY$$$$YYiiiiiYYYYYYi'
-`.replace(/Y+/g, (p) => w(p)).replace(/\$+/g, (p) => Y(p)))
+`
+      .replace(/Y+/g, p => w(p))
+      .replace(/\$+/g, p => Y(p))}`
+  )
 
-    cb(null, doge)
-  }))
+  cb(null, doge)
+}
+
+if (process.stdin.isTTY) {
+  const input = process.argv.slice(2).join(`\n`) || "\n\nohai\n\n\nwow\nno input\n\n\n\n\n\namaze"
+  return dogesay(input, (err, output) => console.log(output))
+}
+
+process.stdin
+  .pipe(utf8())
+  .pipe(map((buf, cb) => cb(null, buf.toString())))
+  .pipe(map(dogesay))
   .pipe(process.stdout)
